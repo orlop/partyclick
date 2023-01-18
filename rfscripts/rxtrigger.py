01c92a1ffed61ec7c79d14092df49b7ca3d3a0e1
orlop <orlop.cz@gmail.com>
2023-01-18 10:01:00 +0000

    rx read

--- rfscripts/rxtrigger.py
+++ rfscripts/rxtrigger.py
@@ -7,15 +7,12 @@ rxPin = 10 # Broadcom pin 15 (P1 pin 10) - UART RX
 
 # Pin Setup:
 GPIO.setmode(GPIO.BOARD) # Broadcom pin-numbering scheme    
-GPIO.setup(butPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Button pin set as input w/ pull-up
+GPIO.setup(rxPin, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Button pin set as input w/ pull-up
 
-# Initial state for LEDs:
-GPIO.output(ledPin, GPIO.LOW)
-pwm.start(dc)
-
+# Read GPIO
 print("Here we go! Press CTRL+C to exit")
 try:
     while 1:
-        print(GPIO.input(butPin))
+        print(GPIO.input(rxPin))
 except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
     GPIO.cleanup() # cleanup all GPIO
