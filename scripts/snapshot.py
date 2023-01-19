35aefd7375f974be89b5aafa15c73d251e510747
orlop <orlop.cz@gmail.com>
2023-01-19 08:22:12 +0000

    logic between files

--- scripts/snapshot.py
+++ scripts/snapshot.py
@@ -2,12 +2,16 @@
 # a part of the Diffusionbooth/Partyclick project
 # https://github.com/orlop/partyclick.git
 
+# snapshot.py takes care of the camera logic
+
 # picamera2 is the new libcamera based python library.
 # Required for the proper functionality of Raspberry Pi Camera Module v3
 from picamera2 import Picamera2
 import time
 
-picam2 = Picamera2()
-capture_config = picam2.create_still_configuration()
-picam2.start(show_preview=False)
-picam2.switch_mode_and_capture_file(capture_config, "/home/partyclick/shared/image.jpg")
+# snap() takes a picture and saves it to a shared SMB location
+def snap():
+    picam2 = Picamera2()
+    capture_config = picam2.create_still_configuration()
+    picam2.start(show_preview=False)
+    picam2.switch_mode_and_capture_file(capture_config, "/home/partyclick/shared/image.jpg")
---
+++ scripts/triggers.py
@@ -0,0 +1,9 @@
+# Daniel Severa, Jr., TWK 2023
+# a part of the Diffusionbooth/Partyclick project
+# https://github.com/orlop/partyclick.git
+
+# triggers.py takes care of triggering the process through RF or wired buttons
+
+from snapshot import snap
+
+snap()
