31b22a137000c58a810c8ebf9d358a2f6de19171
orlop <orlop.cz@gmail.com>
2023-01-19 08:16:52 +0000

    SMB fix location

--- scripts/snapshot.py
+++ scripts/snapshot.py
@@ -10,4 +10,4 @@ import time
 picam2 = Picamera2()
 capture_config = picam2.create_still_configuration()
 picam2.start(show_preview=False)
-picam2.switch_mode_and_capture_file(capture_config, "/home/partyclick/partyclickshared/image.jpg")
+picam2.switch_mode_and_capture_file(capture_config, "/home/partyclick/shared/image.jpg")
