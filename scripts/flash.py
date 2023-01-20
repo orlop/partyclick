7c5825167832f1c51b942a043e32c6b8d2911fb8
orlop <orlop.cz@gmail.com>
2023-01-20 17:25:08 +0000

    switch high and low... again

--- scripts/flash.py
+++ scripts/flash.py
@@ -15,7 +15,7 @@ GPIO.setwarnings(False)
 GPIO.setup(21,GPIO.OUT) # BCM pin 21, Board pin 40
 
 print("LED on")
-GPIO.output(21,GPIO.LOW)
+GPIO.output(21,GPIO.HIGH)
 time.sleep(1)
 print("LED off")
-GPIO.output(21,GPIO.HIGH)
+GPIO.output(21,GPIO.LOW)
